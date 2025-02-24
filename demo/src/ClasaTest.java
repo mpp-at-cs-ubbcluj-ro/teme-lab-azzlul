e57721544bae9e9e73bdd88a3f218005dfdeda94
azzlul <150921091+azzlul@users.noreply.github.com>
2025-02-24 10:34:08 +0200

    initial commit

--- demo/src/ClasaTest.java
+++ demo/src/ClasaTest.java
@@ -1,5 +1,6 @@
 public class ClasaTest {
     public static void main(String[] args) {
         System.out.println("Acesta este un mesaj test");
+        System.out.println("Alt mesaj test");
     }
 }
